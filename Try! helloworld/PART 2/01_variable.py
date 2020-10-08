dc720505b531bc03c5ba5f4289b182225917e2ae
이미라 <emira1239@naver.com>
2020-10-08 19:44:55 +0900

    PART.2

---
+++ Try! helloworld/PART 2/01_variable.py
@@ -0,0 +1,5 @@
+name = '이미라'
+print(name)
+
+season = '겨울'
+print('지금은', season, '입니다.')
---
+++ Try! helloworld/PART 2/03_number.py
@@ -0,0 +1,23 @@
+#첫 번째 실습
+a = 33
+b = 3
+
+sum = a + b
+print('sum = ', sum)
+
+sum = a * b
+print('sum = ', sum)
+
+divide = a % b
+print('divide = ', divide)
+
+remainder = a / b
+print('remainder = ', remainder)
+
+power = a ** b
+print('power = ', power)
+
+#두 번째 실습
+birth_year = '1985'
+birth_date = '0502'
+print('year_and_date =', birth_year, birth_date)  #또는 print(birth_year + birth_date)
