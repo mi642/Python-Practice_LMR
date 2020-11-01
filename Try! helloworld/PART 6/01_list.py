97b3727047c3dc303bee4ed06dcb4a827ebf93a0
이미라 <emira1239@naver.com>
2020-11-02 07:57:43 +0900

    201102

---
+++ Try! helloworld/PART 6/01_list.py
@@ -0,0 +1,9 @@
+#첫 번째 실습
+rainbow=['빨강', '주황', '노랑', '초록', '파랑', '보라']
+first_color=rainbow[0]
+print("무지개의 첫 번째 색은 {}이다.".format(first_color))
+
+#두 번째 실습
+rainbow=['빨강', '주황', '노랑', '초록', '파랑', '보라']
+last_color=rainbow[5]
+print("무지개의 마지막 색은 {}이다.".format(last_color))
